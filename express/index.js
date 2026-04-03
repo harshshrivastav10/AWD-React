cd9a4584b145d8033b94d6f02643d00f1e7ed07b
Harsh Shrivastav <harshshrivastav1220@gmail.com>
2026-04-03 04:16:56 +0000

    Express

---
+++ express/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+    res.send("this is default route")
+});
+const port = 3000 || process.env.port;
+app.listen(port, () => {
+    console.log("this is server is running");
+});
